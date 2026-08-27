0b1e057cc0e9881816bcfb9d299fc833d923f5cd
miki-hasic <mhasic@aiopsgroup.com>
2026-08-27 11:56:36 +0300

    #0012 feat: add suggestion picker

---
+++ src/components/SuggestionPicker/SuggestionPicker.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string
+}>()
+
+const suggestions: string[] = ['/genre Electronic', '/style "Tech House"', '/artist Aphex Twin']
+</script>
+
+<template>
+  <div :class="['suggestion-picker', props.class]">
+    <button
+      v-for="suggestion in suggestions"
+      :key="suggestion"
+      type="button"
+      class="btn suggestion-picker-option"
+    >
+      {{ suggestion }}
+    </button>
+  </div>
+</template>
+
+<style scoped src="./SuggestionPicker.scss" lang="scss"></style>
